1c978818c94a2736e2ac457a2935435cf4d81997
krausk <kurz@curse-art.de>
2018-12-11 11:57:18 +0100

    some cleanup done, still bugfixing segfault

--- diffCalcuator.cu
+++ diffCalcuator.cu
@@ -24,31 +24,49 @@ void calcF(int nAtoms, long nDiff,
   double *atomPos3D, size_t atomPos3Dpitch,
   // double *atomDW3D, size_t atomDW3Dpitch,
   double *atomOccu, int *atomType,
-  double *baseFRe, size_t baseFRepitch,
-  double *baseFIm, size_t baseFImpitch,
+  double *atomicF, size_t atomicFpitch,
   double *outFRe, double *outFIm)
 {
-  uint diffIndex = threadIdx.x + (blockDim.x * blockIdx.x);
-  uint atomIndex = threadIdx.y + (blockDim.y * blockIdx.y);
-  // uint atomIndex = threadIdx.x + (blockDim.x * blockIdx.x);
-  // uint diffIndex = threadIdx.y + (blockDim.y * blockIdx.y);
+  long totalThreadsX = (blockDim.x * gridDim.x);
+  long totalThreadsY = (blockDim.y * gridDim.y);
+  int calcManyX = ceil((double)nDiff / totalThreadsX);
+  int calcManyY = ceil((double)nAtoms / totalThreadsY);
 
-  
-  if (atomIndex < nAtoms && diffIndex < nDiff)
+  for (int threadRunX = 0; threadRunX < calcManyX; ++threadRunX)
   {
-    double dotProduct = 0.0;
-    double* thisAtomPos3D = (double*)((char*)atomPos3D + atomIndex * atomPos3Dpitch);
-    double* thisDiffPos3D = (double*)((char*)diffPos3D + diffIndex * diffPos3Dpitch);
-    for (int direction = 0; direction < 3; ++direction)
+    for (int threadRunY = 0; threadRunY < calcManyY; ++threadRunY)
     {
-      dotProduct += thisAtomPos3D[direction] * thisDiffPos3D[direction];
+      long diffIndex = threadIdx.x + (blockDim.x * blockIdx.x) + threadRunX * totalThreadsX;
+      long atomIndex = threadIdx.y + (blockDim.y * blockIdx.y) + threadRunY * totalThreadsY;
+      
+      if (atomIndex < nAtoms && diffIndex < nDiff)
+      {
+        double dotProduct = 0.0;
+        double atomicFExpFact = 0.0;
+        double* thisAtomPos3D = (double*)((char*)atomPos3D + atomIndex * atomPos3Dpitch);
+        double* thisDiffPos3D = (double*)(diffIndex * diffPos3Dpitch + (char*)diffPos3D);
+        double* thisAtomicF = (double*)(atomType[atomIndex] * atomicFpitch + (char*)atomicF);
+        for (int direction = 0; direction < 3; ++direction)
+        {
+          dotProduct += thisAtomPos3D[direction] * thisDiffPos3D[direction];
+          atomicFExpFact += thisDiffPos3D[direction] * thisDiffPos3D[direction];
+        }
+        // Lambda needs to be included here if diffPos3D is not in Q space!
+        atomicFExpFact = atomicFExpFact / (16. * M_PI * M_PI);
+        // not sure if & is needed
+        // might be possible to speed up by summing up block wise and than sum blocks
+        double atomicFRe = 1.0;
+        double atomicFIm = 0.0;
+        // double* thisBaseFIm = (double*)((char*)baseFIm + diffIndex * baseFImpitch);
+        // atomicAdd(&outFRe[diffIndex], (thisBaseFRe[atomType[atomIndex]] * atomOccu[atomIndex] * cos(dotProduct)) );
+        double cosDot = cos(dotProduct);
+        double sinDot = sin(dotProduct);
+        double resultFRe = atomOccu[atomIndex] *((atomicFRe * cosDot)+(atomicFIm * sinDot));
+        double resultFIm = atomOccu[atomIndex] *((atomicFRe * sinDot)+(atomicFIm * cosDot));
+        atomicAdd(&outFRe[diffIndex], resultFRe );
+        atomicAdd(&outFIm[diffIndex], resultFIm );
+      }
     }
-    // not sure if & is needed
-    // might be possible to speed up by summing up block wise and than sum blocks
-    double* thisBaseFRe = (double*)((char*)baseFRe + atomType[atomIndex] * baseFRepitch);
-    atomicAdd(&outFRe[diffIndex], (thisBaseFRe[diffIndex] * atomOccu[atomIndex] * cos(dotProduct)) );
-    double* thisBaseFIm = (double*)((char*)baseFIm + atomType[atomIndex] * baseFImpitch);
-    atomicAdd(&outFIm[diffIndex], (thisBaseFIm[diffIndex] * atomOccu[atomIndex] * sin(dotProduct)) );
   }
 }
 
@@ -60,38 +78,41 @@ int main(int argc, char** argv)
   long nDiff = atol(argv[2]); // along thread.x
   int nAtomTypes = 3; // Fe, O, Ir
 
-  std::cout << 
-    "#Atoms: " << nAtoms << 
-    ", #Diffractions: " << nDiff <<
-    ", #AtomTypes: " << nAtomTypes <<
-    std::endl;
-  std::cout << "MemUsage: " << nDiff*(nAtomTypes+4)*sizeof(double)*2*1e-6 << "MB" << std::endl;
-
-  // int nTx = 32;
   int nTx = 64;
   int nTy = 8;
-  if (nDiff < 32)
+  if (nDiff < nTx)
   {
     nTx = nDiff;
   }
-  if (nAtoms < 8)
+  if (nAtoms < nTy)
   { 
     nTy = nAtoms;
   }
   dim3 threadsPerBlock(nTx, nTy);
-  int nBx = ceil(nDiff / threadsPerBlock.x);
-  int nBy = ceil(nAtoms / threadsPerBlock.y);
+  int nBx = floor(nDiff / threadsPerBlock.x);
+  int nBy = floor(nAtoms / threadsPerBlock.y);
+  if (nBx > 20000)
+  {
+    nBx = 20000;
+  }
   dim3 numBlocks(nBx, nBy);
-  std::cout << "Blocks: " << nBx << " x " << nBy << " Threads: " << nTx << " x " << nTy << std::endl;
 
-  int     *atomType = new int[nAtoms];
-  cudaMallocManaged(&atomType, nAtoms*sizeof(int));
+  std::cout << "#Atoms: " << nAtoms << ", #Diffractions: " << nDiff << ", #AtomTypes: " << nAtomTypes << std::endl;
+  std::cout << "Blocks: " << nBx << " x " << nBy << " Threads: " << nTx << " x " << nTy << std::endl;
+  std::cout << "MemUsage: " << (nDiff*(3+4)*sizeof(double)+nAtoms*4*sizeof(double)+nAtoms*1*sizeof(int)) << "B" << std::endl;
+
+  long nDiffSizeDouble = nDiff*sizeof(double);
+
+  int     *atomType;
+  int     atomTypeValues[nAtoms];
+  gpuErrchk(cudaMalloc(&atomType, nAtoms*sizeof(int)));
+  double  *atomOccu;
+  double  atomOccuValues[nAtoms];
+  gpuErrchk(cudaMalloc(&atomOccu, nAtoms*sizeof(double)));
   double  *atomPos3D;
   double  atomPos3DValues[nAtoms][3];
   size_t  atomPos3Dpitch;
-  cudaMallocPitch(&atomPos3D, &atomPos3Dpitch, 3 * sizeof(double), nAtoms);
-  double  *atomOccu = new double[nAtoms];
-  cudaMallocManaged(&atomOccu, nAtoms*sizeof(double));
+  gpuErrchk(cudaMallocPitch(&atomPos3D, &atomPos3Dpitch, 3 * sizeof(double), nAtoms));
   // TMP Not implemented yet!
   // double  *atomDW3D;
   // double  atomDW3DValues[nAtoms][3] = new double[nAtoms][3];
@@ -100,33 +121,28 @@ int main(int argc, char** argv)
   double  *diffPos3D;
   double  diffPos3DValues[nDiff][3];
   size_t  diffPos3Dpitch;
-  cudaMallocPitch(&diffPos3D, &diffPos3Dpitch, 3 * sizeof(double), nDiff);
+  gpuErrchk(cudaMallocPitch(&diffPos3D, &diffPos3Dpitch, 3 * sizeof(double), nDiff));
   double  *atomicF;
-  double  atomicFValues[nDiff][9];
+  double  atomicFValues[nAtomTypes][9];
   size_t  atomicFpitch;
-  cudaMallocPitch(&atomicF, &atomicFpitch, 9 * sizeof(double), nDiff);
-  // This might take up a lot of RAM:
-  double  *diffFatomicRe;
-  double  diffFatomicReValues[nAtomTypes][nDiff];
-  size_t  diffFatomicRepitch;
-  cudaMallocPitch(&diffFatomicRe, &diffFatomicRepitch, nDiff * sizeof(double), nAtomTypes);
-  double  *diffFatomicIm;
-  double  diffFatomicImValues[nAtomTypes][nDiff];
-  size_t  diffFatomicImpitch;
-  cudaMallocPitch(&diffFatomicIm, &diffFatomicImpitch, nDiff * sizeof(double), nAtomTypes);
-  // static is mainly bulk:
-  double  *diffFstaticRe = new double[nDiff];
-  cudaMallocManaged(&diffFstaticRe, nDiff*sizeof(double));
-  double  *diffFstaticIm = new double[nDiff];
-  cudaMallocManaged(&diffFstaticIm, nDiff*sizeof(double));
-  double  *diffFunitRe = new double[nDiff];
-  cudaMallocManaged(&diffFunitRe, nDiff*sizeof(double));
-  double  *diffFunitIm = new double[nDiff];
-  cudaMallocManaged(&diffFunitIm, nDiff*sizeof(double));
+  gpuErrchk(cudaMallocPitch(&atomicF, &atomicFpitch, 9 * sizeof(double), nAtomTypes));
+  double  *diffFstaticRe;
+  double  diffFstaticReValues[nDiff];
+  gpuErrchk(cudaMallocManaged(&diffFstaticRe, nDiffSizeDouble));
+  double  *diffFstaticIm;
+  double  diffFstaticImValues[nDiff];
+  gpuErrchk(cudaMallocManaged(&diffFstaticIm, nDiffSizeDouble));
+  double  *diffFunitRe;
+  double  diffFunitReValues[nDiff];
+  gpuErrchk(cudaMalloc(&diffFunitRe, nDiffSizeDouble));
+  double  *diffFunitIm;
+  double  diffFunitImValues[nDiff];
+  gpuErrchk(cudaMalloc(&diffFunitIm, nDiffSizeDouble));
 
   // Generate some test data:
+  std::cout << "Init data " << std::endl;
   int newvals1[] = {0,1,1,0,0,1,1,0};
-  std::copy(newvals1, newvals1 + sizeof(newvals1) / sizeof(newvals1[0]), atomType);
+  std::copy(newvals1, newvals1 + sizeof(newvals1) / sizeof(newvals1[0]), atomTypeValues);
   double newvals2[8][3] = {{0.0,0.5,0.0},{0.5,0.5,0.0},{0.0,0.0,0.0},{0.5,0.0,0.0},
                            {0.25,0.5,0.5},{0.75,0.5,0.5},{0.25,0.0,0.5},{0.75,0.0,0.5}};
   for (int index = 0; index < nAtoms; ++index)
@@ -137,34 +153,31 @@ int main(int argc, char** argv)
   };
   double newvals3[] = {0.9, 1.0, 0.9, 1.0,
                        0.9, 1.0, 0.9, 1.0};
-  std::copy(newvals3, newvals3+sizeof(newvals3)/sizeof(newvals3[0]), atomOccu);
+  std::copy(newvals3, newvals3+sizeof(newvals3)/sizeof(newvals3[0]), atomOccuValues);
   for(int index = 0; index < nDiff; ++index)
   {
     diffPos3DValues[index][0] = floor((double)index / 10.);
     diffPos3DValues[index][1] = ceil((double)index / 20.);
     diffPos3DValues[index][2] = (0.1 * (index % 10));
-    // std::copy(newvals4, newvals4+sizeof(newvals4)/sizeof(newvals4[0]), diffPos3D[index]);
-    diffFstaticRe[index] = 0;
-    diffFstaticIm[index] = 0;
-    diffFunitRe[index] = 0;
-    diffFunitIm[index] = 0;
-  };
-  for(int aIndex = 0; aIndex < nAtoms; ++aIndex)
-  {
-    for(int qIndex = 0; qIndex < nDiff; ++qIndex)
-    {
-      diffFatomicReValues[aIndex][qIndex] = 1.0;
-      diffFatomicImValues[aIndex][qIndex] = 1.0;
-    };
+
+    diffFstaticReValues[index] = 0.0;
+    diffFstaticImValues[index] = 0.0;
+    diffFunitReValues[index] = 0.0;
+    diffFunitImValues[index] = 0.0;
   };
 
   // copy
+  std::cout << "Copy memory " << std::endl;
+  gpuErrchk(cudaMemcpy(atomType, atomTypeValues, nAtoms*sizeof(int), cudaMemcpyHostToDevice));
+  gpuErrchk(cudaMemcpy(atomOccu, atomOccuValues, nAtoms*sizeof(double), cudaMemcpyHostToDevice));
+  gpuErrchk(cudaMemcpy2D(atomicF, atomicFpitch, atomicFValues, 9*sizeof(double), 9*sizeof(double), nAtomTypes, cudaMemcpyHostToDevice));
   gpuErrchk(cudaMemcpy2D(atomPos3D, atomPos3Dpitch, atomPos3DValues, 3*sizeof(double), 3*sizeof(double), nAtoms, cudaMemcpyHostToDevice));
   // cudaMemcpy2D(atomDW3D, atomDW3Dpitch, atomDW3DValues, 3*sizeof(double), 3*sizeof(double), nAtoms, cudaMemcpyHostToDevice);
-  cudaMemcpy2D(diffPos3D, diffPos3Dpitch, diffPos3DValues, 3*sizeof(double), 3*sizeof(double), nDiff, cudaMemcpyHostToDevice);
-  cudaMemcpy2D(atomicF, atomicFpitch, atomicFValues, 9*sizeof(double), 9*sizeof(double), nDiff, cudaMemcpyHostToDevice);
-  cudaMemcpy2D(diffFatomicRe, diffFatomicRepitch, diffFatomicReValues, nDiff*sizeof(double), nDiff*sizeof(double), nAtomTypes, cudaMemcpyHostToDevice);
-  cudaMemcpy2D(diffFatomicIm, diffFatomicImpitch, diffFatomicImValues, nDiff*sizeof(double), nDiff*sizeof(double), nAtomTypes, cudaMemcpyHostToDevice);
+  gpuErrchk(cudaMemcpy2D(diffPos3D, diffPos3Dpitch, diffPos3DValues, 3*sizeof(double), 3*sizeof(double), nDiff, cudaMemcpyHostToDevice));
+  gpuErrchk(cudaMemcpy(diffFstaticRe, diffFstaticReValues, nDiffSizeDouble, cudaMemcpyHostToDevice));
+  gpuErrchk(cudaMemcpy(diffFstaticIm, diffFstaticImValues, nDiffSizeDouble, cudaMemcpyHostToDevice));
+  gpuErrchk(cudaMemcpy(diffFunitRe, diffFunitReValues, nDiffSizeDouble, cudaMemcpyHostToDevice));
+  gpuErrchk(cudaMemcpy(diffFunitIm, diffFunitImValues, nDiffSizeDouble, cudaMemcpyHostToDevice));
   
   std::cout << "Start calcF " << std::endl;
   // Run 
@@ -173,20 +186,21 @@ int main(int argc, char** argv)
     atomPos3D, atomPos3Dpitch,
     // atomDW3D, atomDW3Dpitch,
     atomOccu, atomType,
-    diffFatomicRe, diffFatomicRepitch,
-    diffFatomicIm, diffFatomicImpitch,
+    atomicF, atomicFpitch,
     diffFunitRe, diffFunitIm);
 
   // Wait for GPU to finish before accessing on host
   gpuErrchk(cudaDeviceSynchronize());
 
+  gpuErrchk(cudaMemcpy(diffFunitReValues, diffFunitRe, nDiffSizeDouble, cudaMemcpyDeviceToHost));
+  gpuErrchk(cudaMemcpy(diffFunitImValues, diffFunitIm, nDiffSizeDouble, cudaMemcpyDeviceToHost));
   // Test
   float output = 0.0;
   std::cout << std::fixed;
   std::cout.precision(2);
   for(int index = 0; index < 100; ++index)
   {
-    output = (pow(diffFunitRe[index], 2) + pow(diffFunitIm[index], 2));
+    output = (pow(diffFunitReValues[index], 2) + pow(diffFunitImValues[index], 2));
     std::cout << output << " ";
     if ((index+1) % 10 == 0) 
     {
@@ -202,8 +216,6 @@ int main(int argc, char** argv)
   cudaFree(atomOccu);
   cudaFree(diffPos3D);
   cudaFree(atomicF);
-  cudaFree(diffFatomicRe);
-  cudaFree(diffFatomicIm);
   cudaFree(diffFstaticRe);
   cudaFree(diffFstaticIm);
   cudaFree(diffFunitRe);
